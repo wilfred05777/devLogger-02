826633331a14de659253eaada446200aef17f729
wilfred <wilfred@evodynamic.com>
2020-07-01 05:57:30 +0800

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponent,
+    LogsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
